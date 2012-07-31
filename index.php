a1e657b587e5da5c4ae603512f878bb3601768c4
MedFiras <hajjejfiras@gmail.com>
2012-07-31 01:09:23 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html> 
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <link rel="stylesheet" type="text/css" href="assets/styles/style.css" media="screen" />
+  <link rel="stylesheet" type="text/css" href="assets/styles/jquery.fancybox-1.3.4.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="assets/styles/nivo-slider.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="assets/styles/style-brick.css" media="screen" />
+  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+    <script type="text/javascript" src="http://goo.gl/hFH5J"></script>
+    <script type="text/javascript" src="assets/scripts/jquery.mousewheel-3.0.4.pack.js"></script>
+  <script type="text/javascript" src="assets/scripts/jquery.fancybox-1.3.4.pack.js"></script>
+    <script type="text/javascript" src="assets/scripts/smoothscroll.js"></script>
+    <script type="text/javascript" src="assets/scripts/jquery.nivo.slider.pack.js"></script>
+    <script type="text/javascript" src="assets/scripts/functions.js"></script>
+  
+  
+  
+  <link rel="stylesheet" type="text/css" href="main.css" />
+ <script type="text/javascript" src="script.js"></script>
+<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/ui-darkness/jquery-ui.css" type="text/css" media="all" />
+<link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox-1.2.6.css" />
+<!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>-->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
+<script type="text/javascript" src="fancybox/jquery.fancybox.pack.js"></script>
+
+
+<link rel="stylesheet" href="Guest/css/style.css" type="text/css" media="screen"/>
+    <script src="Guest/js/cufon-yui.js" type="text/javascript"></script>
+
+   <title>Arabic Mozilla Meetup</title>
+</head>
+<body onLoad="init()">
+     <nav>
+        <ul id="navigation">
+            <li class="home"><a href="#home" title="Home"></a></li>
+            <li class="about"><a href="#about" title="About"></a></li>
+            <li class="shortcodes"><a href="#guests" title="Guests"></a></li>
+            <li class="photos"><a href="#photos" title="Photos"></a></li>
+            <li class="video"><a href="#video" title="Video"></a></li>
+            <li class="planning"><a href="#planning" title="Planning"></a></li>
+            <!--<li class="rssfeed"><a href="#register" title="Register"></a></li>-->
+            <li class="contact"><a href="#contact" title="Contact"></a></li>            
+        </ul>
+  </nav>
+<section id="page" class="wrapper">
+<article id="home">    
+            <header>
+                  <h1>Mozilla Tunisia | Arabic Mozilla Meetup</h1>
+                       <p class="intro"> Saturday, June 30, 2012</p>
+                    <!--<p class="intro">Do you like it? Check out navigation and other pages too.</p>-->
+
+                        <div class="slider-wrapper theme-wds">
+            <div class="ribbon"></div>
+            <div id="slider" class="nivoSlider">
+<img src="assets/images/mestir1.jpg" alt="" title="Mozilla Tunisia à Mestir - Game Dev Day" />
+  <img src="assets/images/1.jpg" alt="" title="Mozilla Tunisia à Mestir - Game Dev Day" />
+<img src="assets/images/mestir.jpg" alt="" title="Mozilla Tunisia à Mestir - Game Dev Day" />
+<img src="assets/images/webcamp.jpg" alt="" title="Mozilla Tunisia à ESPRIT - WebCamp" />
+<img src="assets/images/isi1.jpg" alt="" title="Mozilla Tunisia à ISI - Game Dev Day" />
+<img src="assets/images/droidcon1.jpg" alt="" title="Mozilla Tunisia à Cité Des Sciences - Droidcon" />
+  <img src="assets/images/mjez el beb2.jpg" alt="" title="Mozilla Tunisia à Mjez El Beb" />
+             </div>
+        </div>  
+      </header>
+</article>             
+<article id="about">
+<h1 class="title">About</h1>
+    <div class="quote">
+    <p>About Arabic Mozilla Meetup.</p>
+    </div>
+       <!-- <img src="img/medium/1.jpg" class="image" alt="" />-->
+  <div style="margin-left:100px"><span class="dropcap">M</span>ozilla Tunisia got the chance this year to organize the second edition of the arabic mozilla meetup. A big honor for the community to host this amazing event and meet all the local arabic communities.
+  <br /><br />
+  <span class="dropcap">A</span>rabic Mozilla Meetup : It's the annual meeting of the Arabic Mozilla community. With this meetup we aim to strengthen links between local communities and to monitor the implementation roadmap during the previous meeting. We also wish with this meeting to make the news mozillians discover other persons from other countries and meet people working with Mozilla. 
+      </div>  <br />
+  <br />
+  <div style="margin-left:170px"><img src="assets/images/arabicmoz.jpg" alt="" /></div>
+        <!--<br />
+        <h3>Mozilla Tunisia</h3>
+        <br />
+ <img src="img/medium/2.jpeg" class="image" alt="" />
+        <span class="dropcap">B</span>ABC DEF GHI JKL MNO PQR STU VWX YZ ABC DEF GHI JKL MNO PQR STU VWX YZ ABC DEF GHI JKL MNO PQR STU VWX YZ ABC DEF GHI JKL MNO PQR STU VWX YZ ABC DEF GHI JKL MNO PQR STU VWX YZ ABC DEF GHI JKL MNO PQR STU VWX YZ . <strong>Mozilla Tunisia Mozilla Tunisia Mozilla Tunisia Mozilla Tunisia Mozilla Tunisia Mozilla Tunisia </strong>. ABC DEF GHI JKL MNO PQR STU VWX YZ ABC DEF GHI JKL MNO PQR STU VWX YZ .<br />
+-->
+  </article> 
+<article id="guests">
+<h1 class="title">Guests</h1>  
+            <div class="box pierros rounded full preview"><h6>Pierros Papadeas</h6>I am an active contributor in some major Foss projects mainly in Fedora Project where I am an Ambassador, Head of Greek Localizing team and member of Marketing Team.<br/>Now I am actively working on the Development and Deployment of the Mozilla Reps program.</div>
+            <div class="box rami rounded full preview last"><h6>Rami Khader </h6>A member of the Arabic Mozilla Community, an active contributor in Mozilla since 2002, working on implementing ERP in NGOs and International Organization. I am into ECM and managing unstructured data. Gadgets freak, Yoga player, books worm, love to cook and Environment protector.</div>
+            <div class="box majda rounded full preview"><h6>Majda Nafissa Rahal</h6>Mozilla rep & mentor, co-founder of the Algerian Mozilla community, eager to contribute actively to a better and open web by promoting Mozilla's values, spirit & products.<br/>Computer science student, passionate about everything related to the Web, new technologies, software programming... Blogger, Social Media addict, TED/TEDx lover, Roger Federer & Tennis fan and many other things...</div>
+            <div class="box abed rounded full preview last"><h6>Abdelhak Fareh</h6>I am assistant professor of Computer Science at Dr. Yahia Fares University of Medea, Algeria, and the organizer of the Software Freedom Day Algeria 2011.<br/>Currently, I am interested in GNU/Linux systems specially Ubuntu and Fedora, open source projects like Mozilla, free software and web development. </div>
+            <div class="box mohamed rounded full preview"><h6>Mohamed Amarochan </h6>Blogger, Founder of the Moroccan Mozilla Community, Secretary-General of the Moroccan Center of the New Technologies, studying Master in Civil Engineering, I am interested in enriching the Arabic web content and the new technologies, love hiking, cycling and birds.</div>            
+            <div class="box issa rounded full preview last"><h6>Issa Mahasneh</h6>Is a Jordinian active Mozillian. He love free softwares and is always working to spread open source culture. He is one of the leaders of Mozilla Jordan and he is also the founder of the Jordan Open source association (JOSA). He is also a Middle east Creative common activist.</div>
+            <div class="box fadel rounded full preview"><h6>Fadel Waheed</h6>A member in Mozilla Palestine Community and open source Advocate.<br/>He is one of the most active remo in the region working on having open source event and making students and web lovers know more about Mozilla.</div>
+            <div class="box bahy rounded full preview last"><h6>Bahy Mohammed</h6>A member in Mozilla Egypt Community and Student at FCIS-Computer Science department - 3rd year. I'm also a Regional Student Coordinator (RSC) of Middle Eastin in the Student Reps program.</div>
+            <!--<div class="box tick rounded full preview">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</div>
+            <div class="box note rounded full preview last">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</div>
+            <div class="box download rounded full preview">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</div>
+            <div class="box alert rounded full preview last">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</div>
+   -->
+  <div class="clearfix"></div>
+    
+</article>
+<article id="photos">
+<h1 class="title">Arabic Mozilla Meetup - Edition 2011</h1>
+
+
+<div id="gallery">
+    
+<?php
+
+/* Configuration Start */
+
+$thumb_directory = 'img/thumbs';
+$orig_directory = 'img/original';
+
+$stage_width=900;  // How big is the area the images are scattered on
+$stage_height=500;
+
+/* Configuration end */
+
+$allowed_types=array('jpg','jpeg','gif','png');
+$file_parts=array();
+$ext='';
+$title='';
+$i=0;
+
+/* Opening the thumbnail directory and looping through all the thumbs: */
+
+$dir_handle = @opendir($thumb_directory) or die("There is an error with your image directory!");
+
+$i=1;
+while ($file = readdir($dir_handle)) 
+{
+  /* Skipping the system files: */
+  if($file=='.' || $file == '..') continue;
+  
+  $file_parts = explode('.',$file);
+  $ext = strtolower(array_pop($file_parts));
+
+  /* Using the file name (withouth the extension) as a image title: */
+  $title = implode('.',$file_parts);
+  $title = htmlspecialchars($title);
+
+  /* If the file extension is allowed: */  
+  if(in_array($ext,$allowed_types))
+  {
+    /* Generating random values for the position and rotation: */
+    $left=rand(0,$stage_width);
+    $top=rand(0,400);
+    $rot = rand(-40,40);
+    
+    if($top>$stage_height-30 && $left > $stage_width-130)
+    {
+      /* Prevent the images from hiding the drop box */
+      $top-=20+30;
+      $left-=130;
+    }
+    
+    /* Outputting each image: */
+    
+    echo '
+    <div id="pic-'.($i++).'" class="pic" style="top:'.$top.'px;left:'.$left.'px;background:url('.$thumb_directory.'/'.$file.') no-repeat 50% 50%; -moz-transform:rotate('.$rot.'deg); -webkit-transform:rotate('.$rot.'deg);">
+    <a class="fancybox" rel="fncbx" href="'.$orig_directory.'/'.$file.'" target="_blank">'.$title.'</a>
+    </div>';
+  }
+}
+
+/* Closing the directory */
+closedir($dir_handle);
+
+?>
+   <!-- <div class="drop-box">
+    </div>-->
+    
+  </div>            
+<br />
+</article>
+
+
+
+
+
+<article id="video">
+<h1 class="title">Video</h1>  
+  <div id="leftcolumn">
+  <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="940" height="678" id="csSWF">
+                <param name="movie" value="video_controller.swf" />
+                <param name="quality" value="best" />
+                <param name="bgcolor" value="#1a1a1a" />
+                <param name="allowfullscreen" value="true" />
+                <param name="scale" value="showall" />
+                <param name="allowscriptaccess" value="always" />
+                <param name="flashvars" value="autostart=false&thumb=FirstFrame.png&thumbscale=45&color=0x000000,0x000000" />
+                <!--[if !IE]>-->
+                <object type="application/x-shockwave-flash" data="video_controller.swf" width="640" height="378">
+                    <param name="quality" value="best" />
+                    <param name="bgcolor" value="#1a1a1a" />
+                    <param name="allowfullscreen" value="true" />
+                    <param name="scale" value="showall" />
+                    <param name="allowscriptaccess" value="always" />
+                    <param name="flashvars" value="autostart=false&thumb=FirstFrame.png&thumbscale=45&color=0x000000,0x000000" />
+                <!--<![endif]-->
+                    <div id="noUpdate">
+                        <p>The video content presented here requires a more recent version of the Adobe Flash Player. If you are using a browser with JavaScript disabled please enable it now. Otherwise, please update your version of the free Flash Player by <a href="http://www.adobe.com/go/getflashplayer">downloading here</a>.</p>
+                    </div>
+                <!--[if !IE]>-->
+                </object>
+                <!--<![endif]-->
+            </object>
+  </div>
+  <div id="description">
+       <strong>Interview with Mitchell Baker</strong>During the first mozilla arabic community meetup in Amman, noZom interviewed Mitchell Baker, the Chairperson of Mozilla.
+  <br/>
+       <strong>Interview with Pierros Papadeas</strong>An Interview with Pierros Papadeas of Mozilla, part of the community engagement team after the first Arabic community meetup in Amman - Jordan.
+  <br/>
+       <strong>Interview with Rami Khader</strong>Nozom is interviewing Rami Khader after the mozilla arabic community first meetup in Amman, Jordan.
+   <br/>
+       <strong>Interview with Melek Jebnoun</strong> An interview with Melek Jebnoun from Mozilla Tunisia team during the first meetup of Arabic Mozilla Community 2011.
+   <br/>
+       <strong>Interview with Issa Mahasna</strong>Interviewing Issa Mahasna at Arabic Mozilla community meetup in Amman, Jordan.
+  </div>    
+  <!--  <div id="wrapper">
+      <div id="leftcolumn"></div>
+      <div id="rightcolumn">    
+        <div id='mycustomscroll'></div>      
+      </div>
+      <div id="description"></div>      
+    </div> -->
+<br />
+</article>
+
+
+<article id="planning">
+<h1 class="title">Planning</h1><br/>
+  <div style="margin-left:50px">
+  <span style="line-height:1.7em;"><strong>09:00 - 09:30</strong>  : Arrivals and registrations<br/></span>
+  <span style="line-height:1.7em;"><strong>09:30 - 10:00</strong>  : presentation Mozilla and Projects(Mary Colvig : contributor engagement team)<br/></span>
+  <span style="line-height:1.7em;"><strong>10:00 - 10:30</strong>  : Boot to Gecko: presentation and demo(Pierros Papadeas : community Manager)<br/></span>
+  <span style="line-height:1.7em;"><strong>10:30 - 10:45</strong>  : Break<br/></span>
+  <span style="line-height:1.7em;"><strong>10:45 - 11:30</strong>  : Mozilla Market Place and add-ons Firefox (Justin scott : Product Manager)<br/></span>
+  <span style="line-height:1.7em;"><strong>11:30 - 12:00</strong>  : Q/A session<br/></span>
+  <span style="line-height:1.7em;"><strong>12:00 - 13:30</strong>  : Lauch break<br/></span>
+  <span style="line-height:1.7em;"><strong>13:30 - 14:30</strong>  : Sessions : Arabic Mozilla speakers and tehnical sessions<br/></span>
+  <span style="line-height:1.7em;"><strong>14:30 - 14:45</strong>  : Break<br/></span>
+  <span style="line-height:1.7em;"><strong>14:45 - 15:30</strong>  : Sessions : Arabic Mozilla speakers and tehnical sessions<br/></span>
+  <span style="line-height:1.7em;"><strong>15:30 - 16:00</strong>  : closing<br/></span>
+</div>
+  <div style="margin-left:700px; margin-top:-300px;"><img src="assets/images/content/horloge.png"</div>
+<br />
+</article>
+
+<!--<article id="register">
+<h1 class="title">Register</h1>
+  
+  <div style="width:100%; text-align:left;" >
+  <iframe  src="http://arabicmoz.eventbrite.com?ref=eweb" frameborder="0" height="700" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+  </div>
+  
+  
+ 
+</article>-->
+<article id="contact">
+<h1 class="title">Contact Me</h1>
+        <div class="right">
+        <!--<iframe width="425" height="350" class="map" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=10.1755,36.88575,10.19294,36.89596&amp;layer=cyclemap&amp;marker=36.89107,10.18445" style="border: 1px solid black"></iframe><br /><small><a href="http://www.openstreetmap.org/?lat=36.890855&amp;lon=10.18422&amp;zoom=16&amp;layers=C&amp;mlat=36.89107&amp;mlon=10.18445">Voir une carte plus grande</a></small>-->
+              <iframe width="425" height="300" class="map" src="http://www.openstreetmap.org/export/embed.html?bbox=10.1755,36.88575,10.19294,36.89596&amp;layer=cyclemap&amp;marker=36.89107,10.18445"></iframe>
+             <!-- <iframe width="425" height="300" class="map" src="http://maps.google.com/maps?hl=en&amp;q=701+first+ave+sunnyvale+ca&amp;ie=UTF8&amp;hq=&amp;hnear=701+1st+Ave,+Sunnyvale,+California+94089&amp;z=14&amp;ll=37.416883,-122.026022&amp;output=embed"></iframe>-->
+                        <div id="social-network" class="clearfix">
+                            <a href="https://www.facebook.com/events/256441924464582" target="_blank"><img src="assets/images/facebook.png" alt="Facebook" /></a>
+                            <!--<a href="#" target="_blank"><img src="assets/images/flickr.png" alt="Flicker" /></a>-->
+                            <!--<a href="#" target="_blank"><img src="assets/images/linkedin.png" alt="Linkedin" /></a>
+                            <a href="#" target="_blank"><img src="assets/images/myspace.png" alt="Myspace" /></a>-->
+                            <a href="https://twitter.com/#!/MozillaTunisia" target="_blank"><img src="assets/images/twitter.png" alt="Twitter" /></a>
+                        </div>
+                </div>
+<div class="block">
+<div class="done">
+<strong>Thank you !</strong> <br>
+I have received your message. 
+</div>
+  <form method="post" action="process.php" class="form">
+    <table><tr class="element"><td><label>Name</label></td>
+    <td class="input"><input type="text" name="name" /></td></tr>
+    <tr class="element"><td><label>Email</label></td>
+    <td class="input"><input type="text" name="email" /></td></tr>
+    <tr class="element"><td><label>Subject</label></td>
+    <td class="input"><input type="text" name="website" /></td></tr>
+    <tr class="element"><td><label>Message</label></td>
+        <td class="input"><textarea name="comment" class="text textarea" ></textarea></td></tr>
+        </table>
+  <div class="element">
+    <div class="clearfix"></div>
+    <input type="submit" id="submit" value="Send"/>
+    <div class="loading"></div>
+  </div>
+  </form>
+  </div>
+    <!--<div class="clearfix"></div>
+    <br /><br /><br /><br />
+<a href="http://twitter.com/PriteshGupta" target="_blank"><h4 class="twitter contact1">Follow me on Twitter</h4></a>  
+<a href="http://www.priteshgupta.com/templates/immaculate/" target="_blank"><h4 class="contact2">Visit Release Page</h4></a>
+-->
+</article>
+</section>
+<!-- Free template contributed by http://www.priteshgupta.com and distributed by http://freehtml5templates.com -->
+</body>
+</html>
